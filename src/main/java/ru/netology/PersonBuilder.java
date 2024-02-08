c06f399e4621e4cc8210a2c5a0bb756c1d48114e
Roman Milkov <mir3227@mail.ru>
2024-02-08 22:51:28 +0500

    First

---
+++ src/main/java/ru/netology/PersonBuilder.java
@@ -0,0 +1,45 @@
+package ru.netology;
+
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private int age = -1;
+    private String address;
+
+    public PersonBuilder setName(String name) throws IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name not specified");
+        } else this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) throws IllegalArgumentException {
+        if (surname == null || surname.isEmpty()) {
+            throw new IllegalArgumentException("Surname not specified");
+        } else this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) throws IllegalArgumentException {
+        if (age < 0) {
+            throw new IllegalArgumentException("The age is not allowed");
+        } else this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() throws IllegalStateException {
+        Person person;
+        if (name == null || surname == null)
+            throw new IllegalStateException("The first or last name is not specified");
+        if (age < 0) {
+            person = new Person(name, surname);
+        } else person = new Person(name, surname, age);
+        person.setAddress(address);
+        return person;
+    }
+}
